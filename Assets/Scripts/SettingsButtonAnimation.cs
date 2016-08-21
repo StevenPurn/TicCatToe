16dc5a98cf634c8ffb14d08560da8117a3d36730
oops-accidental <smp5119@gmail.com>
2016-08-21 14:08:34 -0700

    Update UI
    - New Cat/Cheese Sprites
    - New BG Image
    - Settings Menu

---
+++ Assets/Scripts/SettingsButtonAnimation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SettingsButtonAnimation : MonoBehaviour {
+
+    public Animation anim;
+    private int animSpeed;
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animation>();
+        animSpeed = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void PlayAnimation()
+    {
+        Debug.Log("Animation Called");
+        anim["SettingsButton"].speed = animSpeed;
+        anim.Play();
+        animSpeed *= -1;
+    }
+}
---
+++ Assets/Scripts/ToggleButtonScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ToggleButtonScript : MonoBehaviour {
+    public enum TypeOfButton { soundEffects, music };
+    public TypeOfButton buttonType;
+    public Image buttonImg;
+    public Sprite onImg;
+    public Sprite offImg;
+    public bool toggleOn = true;
+
+    void Start()
+    {
+        buttonImg = GetComponent<Image>();
+    }
+
+    public void ToggleButton()
+    {
+        if (toggleOn)
+        {
+            if (buttonType == TypeOfButton.music)
+            {
+                Debug.Log("Turn off music");
+            }
+            else
+            {
+                Debug.Log("Turn off sound effects");
+            }
+            buttonImg.sprite = offImg;
+        }
+        else
+        {
+            if (buttonType == TypeOfButton.music)
+            {
+                Debug.Log("Turn on music");
+            }
+            else
+            {
+                Debug.Log("Turn on sound effects");
+            }
+            buttonImg.sprite = onImg;
+        }
+        toggleOn = !toggleOn;
+    }
+}
