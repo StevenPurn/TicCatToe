bddcf97217b84cd3d86145d86d5873683e405c55
oops-accidental <smp5119@gmail.com>
2016-09-05 12:15:43 -0700

    Updated Tile
    Also pictures?

--- Assets/Scripts/Board Scripts/Tile.cs
+++ Assets/Scripts/Board Scripts/Tile.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class Tile {
+public class Tile
+{
     public TileType typeOfTile;
     public TileValue valueOfTile;
     public TileLocation locationOfTile;
--- Assets/Scripts/Network/Logout.cs
+++ Assets/Scripts/Network/Logout.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
-using GameSparks.Api.Requests;
-using Facebook.Unity;
+//using GameSparks.Api.Requests;
+//using Facebook.Unity;
 
 public class Logout : MonoBehaviour {
 
@@ -17,7 +17,7 @@ public class Logout : MonoBehaviour {
 
     public void LogOut()
     {
-        GameSparks.Core.GS.Reset();
-        FB.LogOut();
+        //GameSparks.Core.GS.Reset();
+        //FB.LogOut();
     }
 }
